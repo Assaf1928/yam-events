e370eaecc278862d188859854f83d3e40740ceae
unknown <tzachpony11@gmail.com>
2020-05-04 20:30:44 +0300

    hi

---
+++ src/Components/bigCarousel/bigCarousel.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classes from './bigCarousel.module.css'
+
+const bigCarousel = () => {
+
+    return(
+        <div className={classes.img__div}>
+                bigCarousel
+        </div>
+    );
+}
+
+export default bigCarousel;
