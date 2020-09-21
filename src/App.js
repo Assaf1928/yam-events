0cec38e1e5aafff92b45435b72feae0deb3c108a
unknown <tzachpony11@gmail.com>
2020-09-21 14:28:38 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React from 'react';
+import React from "react";
 //import logo from './logo.svg';
-import { Route, Switch, withRouter} from 'react-router-dom';
-import subCategory from '../src/Containers/SubCategory/SubCategory';
-import Home from  '../src/Containers/Home/Home';
-import './App.css';
+import { Route, Switch, withRouter } from "react-router-dom";
+import subCategory from "../src/Containers/SubCategory/SubCategory";
+import Home from "../src/Containers/Home/Home";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Switch>
-      <Route path="/category" component={subCategory}></Route>
-      <Route path="/home" component={Home}></Route>
-      <Route exact path="/" component={Home}></Route>
-
-      </Switch> 
-         </div>
+        <Route path="/category" component={subCategory}></Route>
+        <Route path="/home" component={Home}></Route>
+        <Route exact path="/" component={Home}></Route>
+      </Switch>
+    </div>
   );
 }
 
