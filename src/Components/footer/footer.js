6bc755fa4225ba90e666aae0a083f5db3914eba0
unknown <tzachpony11@gmail.com>
2020-09-25 20:23:30 +0300

    finished footer

--- src/Components/footer/footer.js
+++ src/Components/footer/footer.js
@@ -4,13 +4,14 @@ import classes from "./footer.module.css";
 import { GrServices } from "react-icons/gr";
 import { AiOutlineClear } from "react-icons/ai";
 import { FaHandsHelping, FaTruckMoving } from "react-icons/fa";
-import Links from './links/links'
+import Links from "./links/links";
 import {
   AiFillFacebook,
   AiFillInstagram,
   AiFillMail,
   AiFillPhone,
 } from "react-icons/ai";
+import classNames from "classnames";
 
 const footer = () => (
   <footer className={classes.footer__container}>
@@ -28,21 +29,33 @@ const footer = () => (
         <AiFillPhone />
       </div>
     </div>
+    <div className={classes.zigzag}></div>
     <div className={classes.bottom}>
-      <div className={classes.flex_item}>
-      <Links/>
+      <div
+        className={classNames({
+          [classes.flex_item]: true,
+          [classes.Links_container]: true,
+        })}
+      >
+        <Links />
+      </div>
+      <div
+        className={classNames({
+          [classes.flex_item]: true,
+          [classes.vl_container]: true,
+        })}
+      >
+        <div className={classes.vl}></div>
       </div>
       <div className={classes.flex_item}>
-      <div className={classes.vl}></div>
+        <div className={classes.credit}>
+          <p>@כל הזכויות שמורות לים אירועים</p>
+          <p>
+            האתר נבנה על ידי : אסף שהרבני assaf1928@gmail.com , צח פוני
+            tzachpony11@gmail.com{" "}
+          </p>
+        </div>
       </div>
-    <div className={classes.flex_item}>
-      <div className={classes.credit}><p>
-@כל הזכויות שמורות לים אירועים
-</p>
-<p>האתר נבנה על ידי אסף שהרבני וצח פוני</p>
-<p>assaf1928@gmail.com</p>
-      </div>
-    </div>
     </div>
   </footer>
 );
