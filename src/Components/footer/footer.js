a8948f9815edcfde0184e7b4eeb28d0f70b4d846
unknown <tzachpony11@gmail.com>
2020-10-17 16:29:29 +0300

    linked contact icons

--- src/Components/footer/footer.js
+++ src/Components/footer/footer.js
@@ -11,22 +11,36 @@ import {
   AiFillMail,
   AiFillPhone,
 } from "react-icons/ai";
+import { RiWhatsappFill } from "react-icons/ri";
 import classNames from "classnames";
 
 const footer = () => (
   <footer className={classes.footer__container}>
     <div className={classes.icons__row}>
       <div>
-        <AiFillFacebook />
+        <a href="https://www.facebook.com/profile.php?id=100004933061558">
+          <AiFillFacebook />
+        </a>
       </div>
       <div>
-        <AiFillInstagram />
+        <a href="https://www.instagram.com/yam1event/">
+          <AiFillInstagram />
+        </a>
       </div>
       <div>
-        <AiFillMail />
+        <a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSHxGzXhpGHGnRqTsjtjscxGQXnqZRdGNjgvJBqgGzXJRlhpRfFBKCBRwGDdNrxwNlRgKSnG">
+          <AiFillMail />
+        </a>
       </div>
       <div>
-        <AiFillPhone />
+        <a href="tel:0523757040">
+          <AiFillPhone />
+        </a>
+      </div>
+      <div>
+        <a href="https://api.whatsapp.com/send?phone=0523757040">
+          <RiWhatsappFill />
+        </a>
       </div>
     </div>
     <div className={classes.zigzag}></div>
